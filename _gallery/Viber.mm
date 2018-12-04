e98558d99fcd4e7b1b295f33705b8b2ec525f8d4
lazar64 <44099621+lazar64@users.noreply.github.com>
2018-12-05 01:23:18 +0200

    Update Viber.mm

--- _gallery/Viber.mm
+++ _gallery/Viber.mm
@@ -1,14 +1,3 @@
----
-author: lazar64
-title: Viber
-image_url: /images/Viber.jpg
-caption: Δημοφιλής εφαρμογή άμεσης ανταλλαγής μηνυμάτων, φωτογραφιών και βίντεο.Υποστηρίζει επίσης συνομιλία μέσω Voip. Λειτουργεί κάτω από δίκτυα  3G/4G  και  Wi/Fi.Η συνομιλία αρχικά προορίζονταν για iphone και android,αργότερα επεκτάθηκε και σε άλλα λειτουργικά Symbian,   
-Windows Phone, Mac OS και τα Microsoft Windows .'
-license_url: ‘https://commons.wikimedia.org/wiki/File:Viber-logo.png’
-license_text: Viber Media Inc.
-categories:
-  - apps
-tags:
-  - communication
----
-
+| author  | title | image_url         | caption                                                                                                                                                                                                                                                                                                                        | license_url                                              | license_text     | categories | tags                |
+|---------|-------|-------------------|--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|----------------------------------------------------------|------------------|------------|---------------------|
+| lazar64 | Viber | /images/Viber.jpg |  Δημοφιλής εφαρμογή άμεσης ανταλλαγής μηνυμάτων, φωτογραφιών και βίντεο. Υποστηρίζει επίσης συνομιλία μέσω Voip. Λειτουργεί κάτω από δίκτυα  3G/4G  και  Wi/Fi. Η συνομιλία αρχικά προορίζονταν για iphone και android,αργότερα επεκτάθηκε και σε άλλα λειτουργικά   Symbian,Windows Phone, Mac OS και τα Microsoft Windows .' | ‘https://commons.wikimedia.org/wiki/File:Viber-logo.png’ | Viber Media Inc. | - apps     | - communication --- |
